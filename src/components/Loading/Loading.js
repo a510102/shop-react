a3c5f4ae4c948f608e5486cb5a29f5e7abfa2821
a510102 <44679115+a510102@users.noreply.github.com>
2020-08-01 00:17:02 +0800

    add h-screen login

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -2,5 +2,5 @@ import React from 'react'
 import './Loading.css'
 
 export default function Loading() {
-  return <div className="load">load</div>
+  return <div className="load h-screen">load</div>
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -67,35 +67,33 @@ export default function Login() {
 
 
   return (
-    <div className='w-full sm:max-w-xs mx-auto mt-4'>
+    <div className='w-full sm:max-w-xs mx-auto mt-4 h-screen'>
       <form onSubmit={handleLogin} className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
         <h2 className="my-1 text-4xl text-teal-500">LogoIn</h2>
-        <div className='my-4'>
-          <label className='block text-teal-500 text-sm font-bold mb-2'> Email :
-        <input
-              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-              type="email"
-              placeholder='email'
-              name='username'
-              onChange={handleChange}
-              value={user.username} />
-          </label>
-        </div>
-        <div className='mb-4'>
-          <label className='block text-teal-500 text-sm font-bold mb-2'> Password :
-        <input
-              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
-              type="current-password"
-              placeholder='password'
-              name="password"
-              onChange={handleChange}
-              value={user.password} />
-          </label>
-        </div>
-        <div className='flex items-center justify-between'>
+        <section className='my-8'>
+          <label className='block text-teal-500 text-sm font-bold mb-2'> Email :</label>
+          <input
+            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+            type="email"
+            placeholder='email'
+            name='username'
+            onChange={handleChange}
+            value={user.username} />
+        </section>
+        <section className='mb-8'>
+          <label className='block text-teal-500 text-sm font-bold mb-2'> Password :</label>
+          <input
+            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+            type="current-password"
+            placeholder='password'
+            name="password"
+            onChange={handleChange}
+            value={user.password} />
+        </section>
+        <section className='flex items-center justify-between'>
           <button className='bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type='submit'>Submit</button>
           <button className='inline-block align-baseline font-bold text-sm text-teal-500 hover:text-teal-800' onClick={() => { history.push('/shop-react/home') }}>Back</button>
-        </div>
+        </section>
       </form>
     </div>
 
